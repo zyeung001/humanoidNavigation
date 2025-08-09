5c30151dd9b2969224a24c5695e508d1e383e051
zyeung001 <zachcnyeung@gmail.com>
2025-08-09 08:06:19 -0700

    Update humanoid_load.py

--- humanoid_load.py
+++ humanoid_load.py
@@ -1,15 +1,24 @@
 import gymnasium as gym
-import numpy as np
+from gymnasium.wrappers import RecordVideo
+from IPython.display import Video
+import os
 
-env = gym.make("Humanoid-v5", render_mode="human")
+# Create environment with video recording wrapper
+env = gym.make("Humanoid-v5", render_mode="rgb_array")
+env = RecordVideo(env, video_folder="./videos/", episode_trigger=lambda x: True)
 
 obs, info = env.reset()
 
-for _ in range(1000):
-    action = env.action_space.sample() 
+for _ in range(500):
+    action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
-
+    
     if terminated or truncated:
         obs, info = env.reset()
 
 env.close()
+
+# Find the video file
+video_files = [f for f in os.listdir("./videos/") if f.endswith('.mp4')]
+if video_files:
+    Video(f"./videos/{video_files[0]}", width=640, height=480)
