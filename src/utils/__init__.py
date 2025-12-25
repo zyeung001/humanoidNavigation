1cf046bcc5d7a7fc93b3f9701ad34bd976664b20
zyeung001 <zachcnyeung@gmail.com>
2025-12-25 14:25:13 -0800

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,10 +5,18 @@ Utility modules for humanoid training.
 Contains:
 - visualization: Display and rendering utilities
 - plot_velocity_commands: Velocity command visualization (Prompt 2)
+- wandb_callbacks: WandB logging for training metrics
 """
 
 from .visualization import setup_display, test_environment
 from .plot_velocity_commands import simulate_and_plot
+from .wandb_callbacks import (
+    VelocityTrackingWandBCallback,
+    CurriculumWandBCallback,
+    VideoRecordingCallback,
+    init_wandb_run,
+    finish_wandb_run
+)
 
 __all__ = [
     # Visualization
@@ -16,4 +24,10 @@ __all__ = [
     'test_environment',
     # Plotting
     'simulate_and_plot',
+    # WandB logging
+    'VelocityTrackingWandBCallback',
+    'CurriculumWandBCallback',
+    'VideoRecordingCallback',
+    'init_wandb_run',
+    'finish_wandb_run',
 ]
